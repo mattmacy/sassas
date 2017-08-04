39c63489039f6b2f32132717eb0ef6d8fa175b3c
Matt Macy <mmacy@mattmacy.io>
2017-08-04 16:39:10 -0700

    generate bindings for Elf headers

---
+++ src/elf/elf.h
@@ -0,0 +1,9 @@
+
+#ifndef	_ELF_H_
+#define	_ELF_H_
+
+#include <elf/types.h>
+#include <elf/elf32.h>
+#include <elf/elf64.h>
+
+#endif /* !_SYS_ELF_H_ */
---
+++ src/elf/types.h
@@ -0,0 +1,7 @@
+#ifndef _ELF_TYPES_H_
+#define _ELF_TYPES_H_
+
+#include <stdint.h>
+typedef uint32_t u_int32_t;
+
+#endif
