90d54eb2afc2928b2bca5051ba167ea9dc0437d9
Matt Macy <mmacy@mattmacy.io>
2017-08-07 15:04:14 -0700

    only expose clippy allow conditionally

--- src/elf.rs
+++ src/elf.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
- #![allow(clippy)]
+ #![cfg_attr(cargo_clippy, allow(clippy))]
+
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
